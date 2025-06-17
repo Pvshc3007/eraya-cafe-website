52689a0f3d86c62557c7cdc679925a25bc5d7a59
Pvshc3007 <maruanajachristinne@gmail.com>
2025-06-17 17:23:30 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Menu from './components/Menu';
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Eraya Cafe Menu</h1>
+      <Menu />
+    </div>
+  );
+}
